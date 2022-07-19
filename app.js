bdaf22151de812842874ab21f0d90b3aae0e1a60
eunkeeee <eunkeeeee@gmail.com>
2022-07-19 19:21:02 +0900

    Updated line width

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
+const lineWidth = document.querySelector("#line-width");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d"); // paint brush
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.lineWidth = 2;
+ctx.lineWidth = lineWidth.value;
 let isPainting = false;
 
 function onMove(event) {
   if (isPainting) {
     // 마우스를 누르고 있으면 => 선을 그리도록 함
+
     ctx.lineTo(event.offsetX, event.offsetY);
     ctx.stroke();
     return;
   }
+  ctx.beginPath();
   // 마우스를 누르고 있지 않으면 => 시작점을 옮기기만 함
   ctx.moveTo(event.offsetX, event.offsetY);
 }
@@ -24,7 +27,12 @@ function startPainting() {
 function cancelPainting() {
   isPainting = false;
 }
+
+function onLineWidthChange(event) {
+  ctx.lineWidth = event.target.value;
+}
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting);
 document.addEventListener("mouseup", cancelPainting);
+lineWidth.addEventListener("change", onLineWidthChange);
