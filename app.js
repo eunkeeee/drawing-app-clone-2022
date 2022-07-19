7a06f7885e93e166bdad604fda3a4a0fd261bc52
eunkeeee <eunkeeeee@gmail.com>
2022-07-19 20:39:36 +0900

    Updated draw, fill mode change

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const lineWidth = document.querySelector("#line-width");
 const color = document.querySelector("#color");
+const colorOptions = Array.from(
+  document.getElementsByClassName("color-option")
+);
+const strokeFill = document.querySelector("#stroke-fill");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d"); // paint brush
 canvas.width = 800;
@@ -7,6 +11,7 @@ canvas.height = 800;
 
 ctx.lineWidth = lineWidth.value;
 let isPainting = false;
+let isFill = false;
 
 function onMove(event) {
   if (isPainting) {
@@ -19,27 +24,49 @@ function onMove(event) {
   // 마우스를 누르고 있지 않으면 => 시작점을 옮기기만 함
   ctx.moveTo(event.offsetX, event.offsetY);
 }
-
 function startPainting() {
   isPainting = true;
 }
-
 function cancelPainting() {
   isPainting = false;
 }
-
 function onLineWidthChange(event) {
   ctx.lineWidth = event.target.value;
 }
-
+// 매번 두개 다 바꾸기 귀찮으니 한줄로 줄이자!!!
+function ChangeAllColors(color) {
+  ctx.strokeStyle = color;
+  ctx.fillStyle = color;
+}
 function onColorChange(event) {
-  ctx.strokeStyle = event.target.value;
-  ctx.fillStyle = event.target.value;
+  ChangeAllColors(event.target.value);
+}
+function onColorClick(event) {
+  const colorValue = event.target.dataset.color;
+  ChangeAllColors(colorValue);
+  color.value = colorValue; // feedback for user in input
+}
+function changeMode() {
+  if (isFill) {
+    isFill = false;
+    strokeFill.innerText = "Fill";
+  } else {
+    isFill = true;
+    strokeFill.innerText = "Draw";
+  }
+}
+function onCanvasClick() {
+  if (isFill) {
+    ctx.fillRect(0, 0, 800, 800);
+  }
 }
 
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting);
+canvas.addEventListener("click", onCanvasClick);
 document.addEventListener("mouseup", cancelPainting);
 lineWidth.addEventListener("change", onLineWidthChange);
 color.addEventListener("change", onColorChange);
+colorOptions.forEach((color) => color.addEventListener("click", onColorClick));
+strokeFill.addEventListener("click", changeMode);
