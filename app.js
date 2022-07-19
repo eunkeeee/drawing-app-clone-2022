f798c76c50a6f7ea6f5cb8b8e933167369b0b263
eunkeeee <eunkeeeee@gmail.com>
2022-07-19 19:44:56 +0900

    Updated color selection

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const lineWidth = document.querySelector("#line-width");
+const color = document.querySelector("#color");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d"); // paint brush
 canvas.width = 800;
@@ -10,7 +11,6 @@ let isPainting = false;
 function onMove(event) {
   if (isPainting) {
     // 마우스를 누르고 있으면 => 선을 그리도록 함
-
     ctx.lineTo(event.offsetX, event.offsetY);
     ctx.stroke();
     return;
@@ -31,8 +31,15 @@ function cancelPainting() {
 function onLineWidthChange(event) {
   ctx.lineWidth = event.target.value;
 }
+
+function onColorChange(event) {
+  ctx.strokeStyle = event.target.value;
+  ctx.fillStyle = event.target.value;
+}
+
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting);
 document.addEventListener("mouseup", cancelPainting);
 lineWidth.addEventListener("change", onLineWidthChange);
+color.addEventListener("change", onColorChange);
